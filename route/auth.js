c8a19b7d75b54e23f356f5cee3d77803508fd07e
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2017-12-05 01:57:41 +0300

    auth login

--- route/auth.js
+++ route/auth.js
@@ -25,7 +25,38 @@ router.post('/register', async function (req, res, next) {
 });
 
 router.post('/login', async function (req, res, next) {
+    let manager = await db.managers.find({
+        where: {
+            email: req.body.email
+        }
+    });
 
+    if (manager) {
+      let id = manager.id;
+      let status = bcrypt.compareSync(req.body.password, manager.password);        
+      console.log("status " + status);
+        if (status)
+        {
+            console.log("AUTH");
+            console.log("id : " + id);
+            console.log("email : " + manager.email);
+            console.log("password : " + req.body.password);
+            console.log("crypt : " + manager.password);
+            res.end(jwt.sign({
+                id: id,
+                email: req.body.email
+            }, "secret", {expiresIn: 60 * 5}));
+        }
+        else
+        {
+            res.send("AUTH ERROR");
+        }
+
+    }
+    else
+    {
+        res.send("AUTH ERROR");
+    }
 });
 
 module.exports = router;
